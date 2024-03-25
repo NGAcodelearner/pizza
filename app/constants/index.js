911b533a6b355769a1c574033dae815572e79fa1
NGA <r0917268@student.thomasmore.be>
2024-03-25 15:32:06 +0700

    set up

---
+++ app/constants/index.js
@@ -0,0 +1,372 @@
+export const pizzas = [
+  {
+    id: 1,
+    name: "capricciosa",
+    description:
+      "A flavorful blend of ingredients including cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/capricciosa.webp",
+    priceSm: 9.99,
+    priceMd: 10.99,
+    priceLg: 11.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "cheesy",
+    description:
+      "Indulge in gooey cheese goodness topped with cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/cheesy.webp",
+    priceSm: 10.99,
+    priceMd: 11.99,
+    priceLg: 12.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "hawaii",
+    description:
+      "Escape to the tropics with pineapple, ham, cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/hawaii.webp",
+    priceSm: 10.99,
+    priceMd: 11.99,
+    priceLg: 12.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "italian",
+    description:
+      "Enjoy a taste of Italy with cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/italian.webp",
+    priceSm: 11.99,
+    priceMd: 12.99,
+    priceLg: 13.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "margherita",
+    description:
+      "Experience classic simplicity with cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/margherita.webp",
+    priceSm: 9.99,
+    priceMd: 10.99,
+    priceLg: 11.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "pepperoni",
+    description:
+      "Spice things up with savory pepperoni, cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/pepperoni.webp",
+    priceSm: 10.99,
+    priceMd: 11.99,
+    priceLg: 12.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "quattro formaggi",
+    description:
+      "Indulge in a rich blend of four cheeses with cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/quattro-formaggi.webp",
+    priceSm: 12.99,
+    priceMd: 13.99,
+    priceLg: 14.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "quattro stagioni",
+    description:
+      "Savor the flavors of all four seasons with cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/quattro-stagioni.webp",
+    priceSm: 11.99,
+    priceMd: 12.99,
+    priceLg: 13.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "tonno",
+    description:
+      "Delight in the taste of the sea with tuna, cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/tonno.webp",
+    priceSm: 10.99,
+    priceMd: 11.99,
+    priceLg: 12.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "vegetarian",
+    description:
+      "Satisfy your cravings with a medley of fresh vegetables including cherry tomatoes, corn, fresh tomatoes, jalapeno, and parmesan.",
+    image: "/vegetarian.webp",
+    priceSm: 9.99,
+    priceMd: 10.99,
+    priceLg: 11.99,
+    toppings: [
+      {
+        image: "/cherry.png",
+        name: "cherry tomatoes",
+        price: 2,
+      },
+      {
+        image: "/corn.png",
+        name: "corn",
+        price: 2,
+      },
+      {
+        image: "/fresh-tomatoes.png",
+        name: "fresh tomatoes",
+        price: 2,
+      },
+      {
+        image: "/jalapeno.png",
+        name: "jalapeno",
+        price: 2,
+      },
+      {
+        image: "/parmesan.png",
+        name: "parmesan",
+        price: 2,
+      },
+    ],
+  },
+];
